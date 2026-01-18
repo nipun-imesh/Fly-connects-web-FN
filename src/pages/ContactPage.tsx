aaa63aa85ebd4f4268cb3681fe40dde3770d942e
nipun imesh <imeshnipun@gmail.com>
2026-01-18 16:54:05 +0530

    map update

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -256,7 +256,7 @@ export default function ContactPage() {
             <div className="bg-white rounded-xl sm:rounded-2xl shadow-xl overflow-hidden h-[250px] sm:h-[300px] md:h-[400px]">
               <iframe
                 title="Location Map"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387193.30596098735!2d-74.25986548248684!3d40.69714941932609!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2s!4v1647095967555!5m2!1sen!2s"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63338.23!2d80.3611785!3d7.4886199!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zN8KwMjknMTkuMCJOIDgwwrAyMSczOS4xIkU!5e0!3m2!1sen!2s!4v1647095967555!5m2!1sen!2s"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
